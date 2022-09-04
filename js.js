5dcd21a3a6141c0da1007b6782bddd2f557dc837
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-09-04 13:18:12 +0600

    second commit

--- js.js
+++ js.js
@@ -1,1 +1,42 @@
-console.log('js intrigated')
+
+
+const autoLoadCatagory = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log(data.data);
+    displayNewsCatagory(data.data.news_category);
+};
+
+const displayNewsCatagory = (newss) => {
+    console.log(newss);
+    const newsCatagory = document.getElementById('news-catagory');
+    newss.forEach(news => {
+        // console.log(news.category_name);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div onclick="showNewsCardFunction(${news.category_id})"> ${news.category_name}</div>
+        `;
+        newsCatagory.appendChild(div)
+    })
+}
+const showNewsCardFunction = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    // displayNewsCatagory(data.data.news_category);
+    console.log(id)
+}
+autoLoadCatagory();
+
+
+const showNewsCardFunction2 = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    // displayNewsCatagory(data.data.news_category);
+    console.log(id)
+};
+showNewsCardFunction2(2);
