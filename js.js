79ff12f6c009225155ece936bd8889c00a585d7c
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-09-04 16:33:46 +0600

    hoice card full add kora

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 
-
+// first function before catagory 
 const autoLoadCatagory = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
@@ -7,6 +7,7 @@ const autoLoadCatagory = async () => {
     displayNewsCatagory(data.data.news_category);
 };
 
+// displayNewsCatagory d-flex function 
 const displayNewsCatagory = (newss) => {
     console.log(newss);
     const newsCatagory = document.getElementById('news-catagory');
@@ -20,47 +21,97 @@ const displayNewsCatagory = (newss) => {
     })
 };
 
+// card function 
 const showNewsCardFunction = async (id) => {
+
+    // spinner start 
+    spinnerFunction(true);
+
     const url = `https://openapi.programming-hero.com/api/news/category/0${id}`;
     const res = await fetch(url);
     const data = await res.json();
     displayShowNewsCardFunction(data.data);
 };
 
+// card function inner div append
 const displayShowNewsCardFunction = (datas) => {
     console.log(datas);
+
+    // if no data in catagory alert no data 
     if (datas.length === 0) {
+        // spinner end 
+        spinnerFunction(false);
         alert('Sorry no news found, Pls try others catagory.')
         return;
     }
     else {
 
     }
+
     const cardShowContainer = document.getElementById('card-show-container');
     cardShowContainer.textContent = '';
     datas.forEach(data => {
         // console.log(data);
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="row g-0 border m-1">
+        <div class="row g-0 border m-1 p-1">
                         <div class="col-md-4">
-                            <img src="${data.image_url}" class="img-fluid rounded-start" alt="...">
+                            <img  src="${data.image_url}" class="img-fluid rounded" alt="...">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
                                 <h5 class="card-title">${data.title}</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to
-                                    additional content. This content is a little bit longer.</p>
+                                <p class="card-text">${data.details}</p>
                                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                <div>
+                                    <div class="d-flex align-items-center justify-content-between gap-4">
+                                        <div class="d-flex align-items-center justify-content-between gap-2">
+                                            <div class="">
+                                                <img style=" height: 44px"
+                                                    class="img-fluid   rounded-circle border border-danger border-3 text-end"
+                                                    src="${data.author.img}" alt="">
+                                            </div>
+                                            <div class="">
+                                                <div class=" m-0 p-0"><small id="author">${data.author.name}</small></div>
+                                                <div class=" m-0 p-0"><small id="public-date">${data.author.published_date}</small></div>
+                                            </div>
+                                        </div>
+
+                                        <div class=" ">
+                                            <i class="fa-regular fa-eye"></i> <span id=" viesw"> </span> ${data.total_view} <span> M
+                                            </span>
+                                        </div>
+                                        <div class=" ">
+                                            <i class="fa-solid fa-star"></i>
+                                            <i class="fa-regular fa-star-half-stroke"></i>
+                                            <i class="fa-regular fa-star"></i>
+                                            <i class="fa-regular fa-star"></i>
+                                            <i class="fa-regular fa-star"></i>
+                                        </div>
+                                        <div><i class="fa-solid fa-arrow-right"></i></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
         `;
         cardShowContainer.appendChild(div)
     })
-    // console.log(datas);
+
+    // spinner end 
+    spinnerFunction(false);
 };
 
+// spinner function 
+const spinnerFunction = isLoading => {
+    const spinnerDiv = document.getElementById('spinner');
+    if (isLoading) {
+        spinnerDiv.classList.remove('d-none')
+    }
+    else {
+        spinnerDiv.classList.add('d-none')
+    }
+
+}
 autoLoadCatagory();
 showNewsCardFunction(8);
